567a48eaf66be52844709ccc81fb2d6c3ab586a3
naomiboschman <115638225+naomiboschman@users.noreply.github.com>
2024-01-26 14:26:17 +0000

    condooms beschrijving afgemaakt

--- db/create.sql
+++ db/create.sql
@@ -8,14 +8,14 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (name, description, hoeveelheid, code, price)
-VALUES ('durex', 'de classic condoom is in elke maat beschikbaar en een zeer toegankelijke soort voor beginners', '01', '816905633-0', 10.99);
+VALUES ('durex', 'de classic condoom is in elke maat beschikbaar en een zeer toegankelijke soort voor beginners.', '01', '816905633-0', 10.99);
 INSERT INTO products (name, description, hoeveelheid, code, price)
-VALUES ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '01', '077030122-3', 11.00);
+VALUES ('prodoom', 'stimulerende condoom, dit is vooral perfect voor de meiden voor de extra stimulatie. als je extra sensatie wilt ervaren is deze perfect voor jou! het is beschikbaar in dosis van tien. ', '01', '077030122-3', 12.00);
 INSERT INTO products (name, description, hoeveelheid, code, price)
-VALUES ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '01', '445924201-X', 13.50);
+VALUES ('eigen ontworpen condoom', 'je kan nu je eigen condoom ontwerpen, je houdt de classic condoom in de verpakking. Op die verpakking kun je je eigen foto laten maken.', '01', '445924201-X', 13.50);
 INSERT INTO products (name, description, hoeveelheid, code, price)
-VALUES ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '01', '693155505-7', 13.50);
+VALUES ('Sugant: glow in the dark condoom, ', 'hou jij meer van intimiteit in de nacht, dan is deze condoom voor jou! hij geeft namelijk licht in het donker. en geeft je daardoor de ultieme ervaring', '01', '693155505-7', 15.00);
 INSERT INTO products (name, description, hoeveelheid, code, price)
-VALUES ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '01', '686928463-6', 14.00);
+VALUES ('vrouwencondooms, durex', 'heb je als man geen zin om een condoom te dragen? geen probleem we hebben nu hier de vrouwencondoom voor u. of heeft u er niet zo veel vertrouwen in dat de man hem altijd draagt kunt u nu het touw in eigen handen nemen. ', '01', '686928463-6', 10.99);
 INSERT INTO products (name, description, hoeveelheid, code, price)
 VALUES ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '01', '492662523-7', 14.00);
