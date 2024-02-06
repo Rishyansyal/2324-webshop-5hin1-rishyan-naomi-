85dadfb25e6417ffe87337b3a3f3c0436e88c33a
Rishyansyal <115623266+Rishyansyal@users.noreply.github.com>
2024-02-06 12:04:48 +0000

    f

--- db/create.sql
+++ db/create.sql
@@ -19,3 +19,47 @@ INSERT INTO products (name, description, amount, code, price)
 VALUES ('vrouwencondooms, durex', 'heb je als man geen zin om een condoom te dragen? geen probleem we hebben nu hier de vrouwencondoom voor u. of heeft u er niet zo veel vertrouwen in dat de man hem altijd draagt kunt u nu het touw in eigen handen nemen. ',10, '686928463-6',10.99);
 INSERT INTO products (name, description, amount, code, price)
 VALUES ('wiet condoom','heb je zin in meer sensatie tijdens de geslachtsgemeenschap, dan is deze wiet condoom voor jou (alleen voor mensen boven de 18+)',10, '492662523-7', 14.00);
+
+
+CREATE TABLE Merken (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255),
+);
+
+insert into Merken (name)
+values ('durex');
+
+insert into Merken (name)
+values ('prodoom');
+
+insert into Merken (name)
+values ('eigen ontworpen condoom');
+
+insert into Merken (name)
+values ('Sugant');
+
+insert into Merken (name)
+values ('vrouwencondooms');
+
+insert into Merken (name)
+values ('Velvet Goldmine');
+
+Create Tabel Maten (
+  id Integer Primary Key AUTOINCREMENT,
+  name VARCHAR(255),
+  );
+
+Insert into maten (name)
+value ('XS')
+
+Insert into maten (name)
+value ('S')
+
+Insert into maten (name)
+value ('M')
+
+Insert into maten (name)
+value ('L')
+
+Insert into maten (name)
+value ('XL')
