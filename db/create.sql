be3b11939c204990f6b011867228ccb9f6716db6
Rishyansyal <115623266+Rishyansyal@users.noreply.github.com>
2024-02-12 21:33:35 +0000

    sql bestand schoongemaakr

--- db/create.sql
+++ db/create.sql
@@ -1,79 +1,81 @@
-CREATE TABLE products (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  code VARCHAR(15),
-  name VARCHAR(255),
-  amount NUMERIC(10),
-  price NUMERIC(10, 2),
-  description VARCHAR(255)
+CREATE TABLE Products (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ code VARCHAR(15),
+ name VARCHAR(255),
+ amount INTEGER,
+ price NUMERIC(10, 2),
+ description TEXT,
+ brand_id INTEGER,
+ size_id INTEGER,
+ FOREIGN KEY (brand_id) REFERENCES Brands(id), -- Bron MySQL documentatie: https://dev.mysql.com/doc/
+ FOREIGN KEY (size_id) REFERENCES Sizes(id) -- Bron MySQL documentatie: https://dev.mysql.com/doc/
 );
 
-INSERT INTO products (name, description,amount, code, price)
-VALUES ('durex', 'de classic condoom is in elke maat beschikbaar en een zeer toegankelijke soort voor beginners.', 10, '816905633-0',10.99);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('prodoom', 'stimulerende condoom, dit is vooral perfect voor de meiden voor de extra stimulatie. als je extra sensatie wilt ervaren is deze perfect voor jou! het is beschikbaar in dosis van tien. ',10, '077030122-3', 12.00);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('eigen ontworpen condoom', 'je kan nu je eigen condoom ontwerpen, je houdt de classic condoom in de verpakking. Op die verpakking kun je je eigen foto laten maken.',10, '445924201-X', 13.50);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('Sugant: glow in the dark condoom, ', 'hou jij meer van intimiteit in de nacht, dan is deze condoom voor jou! hij geeft namelijk licht in het donker. en geeft je daardoor de ultieme ervaring',10, '693155505-7', 15.00);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('wiet condoom','heb je zin in meer sensatie tijdens de geslachtsgemeenschap, dan is deze wiet condoom voor jou (alleen voor mensen boven de 18+)',10, '492662523-7', 14.00);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('vrouwencondooms, durex', 'heb je als man geen zin om een condoom te dragen? geen probleem we hebben nu hier de vrouwencondoom voor u. of heeft u er niet zo veel vertrouwen in dat de man hem altijd draagt kunt u nu het touw in eigen handen nemen. ',10, '686928463-6',10.99);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('harde condoom','naast de dunne rubbere condoms heb je nu ook de harde siliconen condooms, ze blijven altijd in goeie vorm, reuseable!',10, '492626523-7', 20.00);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('condooms met geur','hou je niet van de geur van rubber of van intieme lichaamsdelen zijn de condooms met geur voor jou, verkijgbaar in aardbeiensmaak.',10, '492664723-7', 15.00);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('Condooms die het klaarkomen uitstellen voor de man.','de naam zegt het al  houd jij of je man het niet zolang vol, dan is deze condoom voor zo heb je lang plezier van de sex',10, '492664273-7', 14.99);
-INSERT INTO products (name, description, amount, code, price)
-VALUES ('Latexvrije condooms.','hou je toch wel van intieme momenten maar ben je allergisch voor latex, hebben we hier de soort voor jou. de condooms die geen latex bevatten',10, '492664273-7', 14.99);
+CREATE TABLE Brands (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name VARCHAR(255)
+);
 
+CREATE TABLE Sizes (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name VARCHAR(255)
+);
 
-CREATE TABLE Merken 
-  (id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(255))
-;
+-- Brands
+INSERT INTO Brands (name) VALUES ('Durex');
 
-insert into Merken (name)
-values ('durex');
+INSERT INTO Brands (name) VALUES ('Prodoom');
 
-insert into Merken (name)
-values ('prodoom');
+INSERT INTO Brands (name) VALUES ('Eigen Ontworpen Condoom');
 
-insert into Merken (name)
-values ('eigen ontworpen condoom');
+INSERT INTO Brands (name) VALUES ('Sugant');
 
-insert into Merken (name)
-values ('Sugant');
+INSERT INTO Brands (name) VALUES ('Vrouwencondooms');
 
-insert into Merken (name)
-values ('vrouwencondooms');
+INSERT INTO Brands (name) VALUES ('Velvet Goldmine');
 
-insert into Merken (name)
-values ('Velvet Goldmine');
+-- Sizes
+INSERT INTO Sizes (name) VALUES ('XXS');
 
-Create TABLE Maten 
-  (id Integer Primary Key AUTOINCREMENT,
-  name VARCHAR(255))
-  ;
+INSERT INTO Sizes (name) VALUES ('XS');
 
-Insert into maten (name)
-values ('XXS');
+INSERT INTO Sizes (name) VALUES ('S');
 
-Insert into maten (name)
-values ('XS');
+INSERT INTO Sizes (name) VALUES ('M');
 
-Insert into maten (name)
-values ('S');
+INSERT INTO Sizes (name) VALUES ('L');
 
-Insert into maten (name)
-values ('M');
+INSERT INTO Sizes (name) VALUES ('XL');
 
-Insert into maten (name)
-values ('L');
+INSERT INTO Sizes (name) VALUES ('XXL');
 
-Insert into maten (name)
-values ('XL');
+-- Products
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Durex Classic Condoom', 'De classic condoom is in elke maat beschikbaar en een zeer toegankelijke soort voor beginners.', 10, '816905633-0', 10.99, 1, 4);
 
-Insert into maten (name)
-values ('XXL');
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Prodoom', 'Stimulerende condoom, dit is vooral perfect voor de meiden voor de extra stimulatie. Als je extra sensatie wilt ervaren is deze perfect voor jou! Het is beschikbaar in dosis van tien.', 10, '077030122-3', 12.00, 2, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Eigen Ontworpen Condoom', 'Je kan nu je eigen condoom ontwerpen. Je houdt de classic condoom in de verpakking. Op die verpakking kun je je eigen foto laten maken.', 10, '445924201-X', 13.50, 3, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Sugant: Glow in the Dark Condoom', 'Hou jij meer van intimiteit in de nacht? Dan is deze condoom voor jou! Hij geeft namelijk licht in het donker en geeft je daardoor de ultieme ervaring.', 10, '693155505-7', 15.00, 4, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Wiet Condoom', 'Heb je zin in meer sensatie tijdens de geslachtsgemeenschap? Dan is deze wiet condoom voor jou (alleen voor mensen boven de 18+).', 10, '492662523-7', 14.00, 5, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Vrouwencondooms, Durex', 'Heb je als man geen zin om een condoom te dragen? Geen probleem, we hebben nu hier de vrouwencondoom voor u. Of heeft u er niet zo veel vertrouwen in dat de man hem altijd draagt kunt u nu het touw in eigen handen nemen.', 10, '686928463-6', 10.99, 5, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Harde Condoom', 'Naast de dunne rubberen condooms heb je nu ook de harde siliconen condooms. Ze blijven altijd in goede vorm en zijn herbruikbaar!', 10, '492626523-7', 20.00, 6, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Condooms met Geur', 'Hou je niet van de geur van rubber of van intieme lichaamsdelen? Zijn de condooms met geur voor jou, verkrijgbaar in aardbeiensmaak.', 10, '492664723-7', 15.00, 1, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Condooms die het Klaarkomen Uitstellen voor de Man', 'De naam zegt het al. Houd jij of je man het niet zo lang vol? Dan is deze condoom voor jou. Zo heb je lang plezier van de seks.', 10, '492664273-7', 14.99, 2, 4);
+
+INSERT INTO Products (name, description, amount, code, price, brand_id, size_id) 
+VALUES ('Latexvrije Condooms', 'Houd je toch wel van intieme momenten maar ben je allergisch voor latex? Hebben we hier de soort voor jou, de condooms die geen latex bevatten.', 10, '492664273-7', 14.99, 3, 4);
